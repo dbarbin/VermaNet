9db915041e2ddef324f7a02b69d57c7d52617c57
Doug Barbin <dbarbin@vermasystem.com>
2017-09-06 10:20:02 -0500

    Initial commit

--- VermaNet.AwesomeBoard/Controllers/HomeController.cs
+++ VermaNet.AwesomeBoard/Controllers/HomeController.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using System.Diagnostics;
+using VermaNet.Data.TigerpawEnhancements;
+using VermaNet.Common;
+
 
 namespace VermaNet.AwesomeBoard.Controllers
 {
@@ -10,21 +11,27 @@ namespace VermaNet.AwesomeBoard.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            int daysInMonth = DateHelper.GetWorkDaysInMonth(DateTime.Now.Month, DateTime.Now.Year);
+            int quarter = (DateTime.Now.Month + 2) / 3;
+            int daysInQuarter = DateHelper.GetWorkDaysInQuarter(quarter, DateTime.Now.Year);
+            return RedirectToAction("LeaderBoard");
         }
 
-        public ActionResult About()
+
+        public ActionResult LeaderBoard(int? WorkDaysInMonth, int? WorkDaysInQuarter)
         {
-            ViewBag.Message = "Your application description page.";
+            // If Work Days in month/quarter were not supplied, figure them out here...
+            WorkDaysInMonth = WorkDaysInMonth.GetValueOrDefault(DateHelper.GetWorkDaysInMonth(DateTime.Now.Month, DateTime.Now.Year));
+            int quarter = (DateTime.Now.Month + 2) / 3;
+            WorkDaysInQuarter = WorkDaysInQuarter.GetValueOrDefault(DateHelper.GetWorkDaysInQuarter(quarter, DateTime.Now.Year));
 
-            return View();
+            // Run stored procedure to return data and bind that data to our View
+            var dbContext = new TigerpawEnhancementsEntities();
+            var model = dbContext.GetAwesomeBoardData(DateTime.Today, WorkDaysInMonth, WorkDaysInQuarter);
+            return View(model);
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
+
     }
 }
---
+++ VermaNet.Common/DateHelper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VermaNet.Common
+{
+    public class DateHelper
+    {
+
+        public static int GetWorkDaysInMonth(int month, int year)
+        {
+            // default to 5/7 of the month arbitrarily...
+            var startDate = new DateTime(year, month, 1);
+            var daysInMonth = DateTime.DaysInMonth(year, month);
+            int result = 0;
+            for(int dayNumber = 1; dayNumber <= daysInMonth; dayNumber++)
+            {
+                var currentDay = new DateTime(year, month, dayNumber);
+                if(new[] { DayOfWeek.Saturday, DayOfWeek.Sunday}.Contains(currentDay.DayOfWeek))
+                {
+                    // Skip weekends
+                    continue;
+                }
+                else
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+
+
+        public static int GetWorkDaysInQuarter(int quarter, int year)
+        {
+            var relevantMonths = new List<int>();
+            switch(quarter)
+            {
+                case 1:
+                    relevantMonths.AddRange(new []{ 1, 2, 3});
+                    break;
+
+                case 2:
+                    relevantMonths.AddRange(new[] { 4, 5, 6 });
+                    break;
+
+                case 3:
+                    relevantMonths.AddRange(new[] { 7, 8, 9 });
+                    break;
+
+                case 4:
+                    relevantMonths.AddRange(new[] { 10, 11, 12 });
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException("quarter", "Invalid quarter was specified.  Valid values are 1, 2, 3, and 4.");
+
+            }
+
+            int result = 0;
+            foreach(int month in relevantMonths.OrderBy(x=> x))
+            {
+                result += GetWorkDaysInMonth(month, year);
+            }
+
+            return result;
+        }
+
+
+    }
+}
